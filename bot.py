cd6c3364253af3c81c131b6ad43c8141a51e1f23
zonti9 <afizulakov@gmail.com>
2023-12-04 15:30:57 +1000

    Добавлена возможность назначить модераторов

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from db import DbWork
-from config import TOKEN
+from config import *
 
 
 class VkBot:
@@ -9,8 +9,8 @@ class VkBot:
         self.vk_session = vk_api.VkApi(token=token)
         self.longpoll = VkLongPoll(self.vk_session)
         self.vk = self.vk_session.get_api()
-        self.db = DbWork()
-        self.commands_to_user = ('/kick', '/warn', '/unwarn', '/mute', '/unmute')
+        self.db = DbWork(*db.values())
+        self.commands_to_user = ('/kick', '/warn', '/unwarn', '/mute', '/unmute', '/addmoder', '/removerole')
         self.ind_commands = ('/warnlist',)
 
     def kick(self, member_id: int, chat_id: int) -> None:
@@ -21,7 +21,7 @@ class VkBot:
 
     def warn(self, member_id: int, member_name: str, chat_id: int, warns: int) -> None:
         warns += 1
-        self.db.set_user_warn(warns, member_id)
+        self.db.set_user_field(member_id, 'warns', warns)
 
         self.vk.messages.send(
             random_id=0,
@@ -34,84 +34,111 @@ class VkBot:
 
     def run(self):
         for event in self.longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW and event.from_chat and event.to_me and event.text:
-                members = self.vk.messages.getConversationMembers(peer_id=2000000000 + event.chat_id)
-                is_admin = sorted(members['items'],
-                                  key=lambda member: member['member_id'] == event.user_id,
-                                  reverse=True)[0].get('is_admin', False)
-                if is_admin:
-                    if (msg := event.text.split())[0] in self.commands_to_user:
-                        member_id = msg[1].split('|')[0][3:]
-                        try:
-                            member_name = self.vk.users.get(user_ids=member_id)[0]['first_name']
-                        except IndexError:
-                            self.vk.messages.send(
-                                random_id=0,
-                                chat_id=event.chat_id,
-                                message='Указан неверный пользователь'
-                            )
-                            continue
-                        member_data = self.db.get_user_data(member_id)
+            if event.type == VkEventType.MESSAGE_NEW and event.from_chat and event.to_me:
+                if event.text:
+                    members = self.vk.messages.getConversationMembers(peer_id=2000000000 + event.chat_id)
+                    condition1 = sorted(members['items'],
+                                        key=lambda member: member['member_id'] == event.user_id,
+                                        reverse=True)[0].get('is_admin', False)
+                    condition2 = self.db.get_user_data(event.user_id)['moderator']
 
-                        match msg[0]:
-                            case '/kick':
-                                self.kick(member_id, event.chat_id)
-                            case '/warn':
-                                self.warn(member_id, member_name, event.chat_id, member_data['warns'])
-                            case '/unwarn':
-                                self.db.set_user_warn(0, member_id)
-
+                    if condition1 or condition2:
+                        if (msg := event.text.split())[0] in self.commands_to_user:
+                            try:
+                                member_id = msg[1].split('|')[0][3:]
+                                member_name = self.vk.users.get(user_ids=member_id)[0]['first_name']
+                            except IndexError:
                                 self.vk.messages.send(
                                     random_id=0,
                                     chat_id=event.chat_id,
-                                    message=f'{member_name} избавлен от предупреждений'
+                                    message='Указан неверный пользователь'
                                 )
-                            case '/mute':
-                                self.db.set_user_mute(True, member_id)
+                                continue
+                            member_data = self.db.get_user_data(member_id)
 
-                                self.vk.messages.send(
-                                    random_id=0,
-                                    chat_id=event.chat_id,
-                                    message=f'{member_name} больше не может отправлять сообщения'
-                                )
-                            case '/unmute':
-                                self.db.set_user_mute(False, member_id)
-
-                                self.vk.messages.send(
-                                    random_id=0,
-                                    chat_id=event.chat_id,
-                                    message=f'{member_name} снова может отправлять сообщения'
-                                )
-                    elif event.text in self.ind_commands:
-                        match event.text:
-                            case '/warnlist':
-                                warnlist = self.db.get_users_with_warns()
-
-                                if warnlist:
-                                    message = ''
-
-                                    for member_id, warns in warnlist:
-                                        member_name = self.vk.users.get(user_ids=member_id)[0]['first_name']
-                                        message += f'{member_name}: {warns}\n'
+                            match msg[0]:
+                                case '/kick':
+                                    try:
+                                        self.kick(member_id, event.chat_id)
+                                    except Exception:
+                                        self.vk.messages.send(
+                                            random_id=0,
+                                            chat_id=event.chat_id,
+                                            message=f'{member_name} уже исключен'
+                                        )
+                                case '/warn':
+                                    self.warn(member_id, member_name, event.chat_id, member_data['warns'])
+                                case '/unwarn':
+                                    self.db.set_user_field(member_id, 'warns', 0)
 
                                     self.vk.messages.send(
                                         random_id=0,
                                         chat_id=event.chat_id,
-                                        message=message
+                                        message=f'{member_name} избавлен от предупреждений'
                                     )
-                                else:
+                                case '/mute':
+                                    self.db.set_user_field(member_id, 'mute', True)
+
                                     self.vk.messages.send(
                                         random_id=0,
                                         chat_id=event.chat_id,
-                                        message='Нет пользователей с предупреждениями'
+                                        message=f'{member_name} больше не может отправлять сообщения'
                                     )
-            elif event.type == VkEventType.MESSAGE_NEW and event.from_chat and event.to_me:
-                member_id = event.user_id
-                member_name = self.vk.users.get(user_ids=member_id)[0]['first_name']
-                member_data = self.db.get_user_data(member_id)
+                                case '/unmute':
+                                    self.db.set_user_field(member_id, 'mute', False)
 
-                if member_data['mute']:
-                    self.warn(member_id, member_name, event.chat_id, member_data['warns'])
+                                    self.vk.messages.send(
+                                        random_id=0,
+                                        chat_id=event.chat_id,
+                                        message=f'{member_name} снова может отправлять сообщения'
+                                    )
+                                case '/addmoder':
+                                    self.db.set_user_field(member_id, 'moderator', True)
+
+                                    self.vk.messages.send(
+                                        random_id=0,
+                                        chat_id=event.chat_id,
+                                        message=f'{member_name} теперь модератор чата'
+                                    )
+                                case '/removerole':
+                                    self.db.set_user_field(member_id, 'moderator', False)
+
+                                    self.vk.messages.send(
+                                        random_id=0,
+                                        chat_id=event.chat_id,
+                                        message=f'{member_name} больше не является модератором чата'
+                                    )
+
+                        elif event.text in self.ind_commands:
+                            match event.text:
+                                case '/warnlist':
+                                    warnlist = self.db.get_users_with_warns()
+
+                                    if warnlist:
+                                        message = ''
+
+                                        for member_id, warns in warnlist:
+                                            member_name = self.vk.users.get(user_ids=member_id)[0]['first_name']
+                                            message += f'{member_name}: {warns}\n'
+
+                                        self.vk.messages.send(
+                                            random_id=0,
+                                            chat_id=event.chat_id,
+                                            message=message
+                                        )
+                                    else:
+                                        self.vk.messages.send(
+                                            random_id=0,
+                                            chat_id=event.chat_id,
+                                            message='Нет пользователей с предупреждениями'
+                                        )
+                else:
+                    member_id = event.user_id
+                    member_name = self.vk.users.get(user_ids=member_id)[0]['first_name']
+                    member_data = self.db.get_user_data(member_id)
+
+                    if member_data['mute']:
+                        self.warn(member_id, member_name, event.chat_id, member_data['warns'])
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,23 +1,25 @@
 from mysql.connector import connect
-from config import db
 
 
 class DbWork:
-    def __init__(self) -> None:
+    def __init__(self, host, user, password, database) -> None:
         self.con = connect(
-            host=db['host'],
-            user=db['username'],
-            password=db['password'],
-            database='vkbot'
+            host=host,
+            user=user,
+            password=password,
+            database=database
         )
-        self.column_names = ('warns', 'mute')
-
+        self.column_names = ('warns', 'mute', 'moderator')
         self.cur = self.con.cursor()
 
     def add_user(self, vk_id: int) -> dict:
         self.cur.execute(f'insert into users (vk_id) values ({vk_id})')
         self.con.commit()
-        return dict(zip(self.column_names, (0, False)))
+        return dict(zip(self.column_names, (0, False, False)))
+
+    def set_user_field(self, vk_id: int, field_name: str, value: int | bool) -> None:
+        self.cur.execute(f'update users set {field_name} = {value} where vk_id={vk_id}')
+        self.con.commit()
 
     def get_user_data(self, vk_id: int) -> dict:
         self.cur.execute(f'select {', '.join(self.column_names)} from users where vk_id={vk_id}')
@@ -26,14 +28,6 @@ class DbWork:
         else:
             return self.add_user(vk_id)
 
-    def set_user_warn(self, warns: int, vk_id: int) -> None:
-        self.cur.execute(f'update users set warns = {warns} where vk_id={vk_id}')
-        self.con.commit()
-
     def get_users_with_warns(self) -> list:
         self.cur.execute('select vk_id, warns from users where warns != 0')
         return self.cur.fetchall()
-
-    def set_user_mute(self, muted: bool, vk_id: int) -> None:
-        self.cur.execute(f'update users set mute = {muted} where vk_id={vk_id}')
-        self.con.commit()
