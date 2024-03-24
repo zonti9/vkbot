46315d51192b77febabe52b9cc1cd221397ebcc7
zonti9 <afizulakov@gmail.com>
2024-03-24 23:44:45 +1000

    отредактировано

--- models.py
+++ models.py
@@ -13,9 +13,6 @@ class Member(Model):
     class Meta:
         table = "members"
 
-    def __str__(self):
-        return self.nick or self.name
-
     def mention(self, use_nick_if_exists: bool):
         if use_nick_if_exists:
             return f"[id{self.vk_id}|{self.nick or self.name}]"
@@ -27,6 +24,6 @@ class Warn(Model):
     member = fields.ForeignKeyField("models.Member", related_name="warns", on_delete=fields.CASCADE)
     active = fields.BooleanField(default=True)
     message = fields.TextField()
-    
+
     class Meta:
         table = "warns"
